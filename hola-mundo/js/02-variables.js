547cd3f763580478c31e10dd71a9fcb73b37f8ec
drey2020 <eyqm2020@gmail.com>
2021-04-22 10:24:41 -0500

    v18. Variables y comentarios

---
+++ hola-mundo/js/02-variables.js
@@ -0,0 +1,15 @@
+//VARIBLE: Es un contenedor de informacion
+
+var pais = "Perú";
+var continente = "América";
+var antiguedad = "1894"
+var pais_y_continente = pais + ' ' + continente;
+
+console.log(pais, continente,antiguedad);
+alert(pais_y_continente);
+
+pais = "Irlanda";
+continente = "Europa";
+
+console.log(pais, continente,antiguedad);
+alert(pais_y_continente);
