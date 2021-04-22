4a8b6467c4adc6baf244817cbcf88632b2a9eccf
drey2020 <eyqm2020@gmail.com>
2021-04-22 12:20:33 -0500

    v21. Constantes

---
+++ hola-mundo/js/04-constantes.js
@@ -0,0 +1,10 @@
+'use strict'
+//Constate: almacena un valor como una variable pero su valor no puede cambiar
+
+var web = "http://cursoJS.es";
+const IP = "192.88.0.12";
+
+web = "cursosJS.com";
+IP = "2256312";
+
+console.log(web, IP);
