bb72a1a1020c3e4475c38c470a76220728efd77b
drey2020 <eyqm2020@gmail.com>
2021-04-22 10:08:06 -0500

    v17. La consola del navegador

--- hola-mundo/hola-mundo.js
+++ hola-mundo/hola-mundo.js
@@ -1,2 +1,4 @@
 alert("Empecemos =D");
 document.write("Estas en el fichero JS") //sirve para excribir en la pagina
+console.log("muestra esto en la consola"); // muestra texto en la consola del navegador
+console.log(48+56);
